bd49f6aedfdd8d960af3127a257aab9e99697fe6
ylanhds <24424233+ylanhds@users.noreply.github.com>
2021-09-08 01:27:52 +0800

    a

---
+++ src/main/java/com/fablen/config/NettyConfig.java
@@ -0,0 +1,14 @@
+package com.fablen.config;
+
+/**
+ * <p>
+ * Description:
+ * </p>
+ *
+ * @author zhangbaosheng
+ * @version v1.0.0
+ * @date 2021-09-08 1:27
+ * @see
+ */
+public class NettyConfig {
+}
